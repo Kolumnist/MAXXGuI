71c564ed130eeef67d5766963e19c0177b3a8144
Kolumnist <96417832+Kolumnist@users.noreply.github.com>
2022-04-20 16:29:22 +0200

    Update MainMenu.java

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -4,11 +4,6 @@ public class MainMenu extends JFrame {
 
     public MainMenu()
     {
-        setLayout(null);
-        setTitle("MAXXGuI");
-        setSize(500, 500);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
 }
