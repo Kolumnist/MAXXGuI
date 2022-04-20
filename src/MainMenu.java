78085077b0f43fbf67231243b26ef46a04f70e90
Kolumnist <96417832+Kolumnist@users.noreply.github.com>
2022-04-20 15:56:54 +0200

    MainMenu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+
 public class MainMenu {
+
+    public MainMenu()
+    {
+        JFrame f = new JFrame();
+        f.setLayout(null);
+        f.setTitle("ElevenColors");
+        f.setSize(width, height);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
 }
