88a3dbbfe90dc0eee2c2d057e048c588843e97b9
Kolumnist <96417832+Kolumnist@users.noreply.github.com>
2022-04-21 12:57:30 +0200

    Button 1

--- src/Button.java
+++ src/Button.java
@@ -1,31 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class Button extends JButton implements MouseListener {
-    
-    @Override
-    public void mouseClicked(MouseEvent e) {
+public class Button extends JButton implements ActionListener {
 
+    public Button(String value)
+    {
+        this.setBackground(Color.RED);
+        this.setName(value);
     }
 
     @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
+    public void actionPerformed(ActionEvent e)
+    {
 
     }
 }
