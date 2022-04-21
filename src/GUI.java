88a4fbf1a9dfe261fc52059a0117cef0f33a313a
Laraisnotavailbale <crazyjasmin9@gmail.com>
2022-04-21 16:02:52 +0200

    Update GUI.java
    GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,2 +1,14 @@
-public class GUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JFrame {
+
+    // GUI für Gameboard
+    public GUI(JButton[] JButton, int rows, int cols, String title) {
+        setTitle(title);
+        setSize(500, 500);
+        setLayout(new GridLayout(rows, cols));
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }
