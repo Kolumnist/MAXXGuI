b56d7486d701d16fa2f87192faa517aa8c16277f
Kolumnist <96417832+Kolumnist@users.noreply.github.com>
2022-04-21 18:40:18 +0200

    MainMenu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,4 +1,3 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,11 +7,17 @@ public class MainMenu extends JFrame implements Runnable {
 
     private JButton play;
     private JComboBox playerSelection;
+    private Player[] players;
 
-    public MainMenu(Player[] player) {
+    public MainMenu(Player[] players) {
         setTitle("MAXXGuI");
         setLayout(new FlowLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        for(Player p : players)
+        {
+            this.players = players;
+        }
 
         // create and add JMenuBar
         JMenuBar menuBar = new JMenuBar();
@@ -66,25 +71,23 @@ public class MainMenu extends JFrame implements Runnable {
         add(play);
 
         //this ActionListener shows the manual
-        manual.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JFrame myJFrame = new JFrame();
-                JPanel panel = new JPanel();
+        manual.addActionListener(e -> {
+            JFrame myJFrame = new JFrame();
+            JPanel panel = new JPanel();
 
-                JTextArea manual = new JTextArea("Hallo! Herzlich willkommen zu 'MAXX'!"
-                        + "\nIn unserem Spiel können vier Spieler abwechselnd miteinander spielen. Diese Spieler sind als 'black', 'white', 'red' und 'yellow' konfiguriert."
-                        + "\nAlle Spieler haben einen Spielstein, welchen sie auf dem Spielfeld hin und her bewegen können."
-                        + "\n\nAber Achtung!!"
-                        + "\nDer Spielstein kann nur in 4 bestimmte Richtungen bewegt werden. Nämlich nach: 'NW' 'NO' 'SO' und 'SW'."
-                        + "\nZudem hat jeder Spieler noch einen special move, welcher dem Schwarzen Spieler erlaubt seinen Spielstein nach rechts zu bewegen, "
-                        + "\ndem weißen Spieler ist es erlaubt, seinen nach links zu bewegen, der rote Spieler kann seinen nach oben und der gelbe Spieler seinen nach unten bewegen."
-                        + "\nDieser move wird unter 'SPECIAL' abgerufen!"
-                        + "\nGewinner des Spiels ist derjenige, der mehr oder gleich 42 Punkte hat!\n");
-                panel.add(manual);
-                myJFrame.setSize(900, 300);
-                myJFrame.add(panel);
-                myJFrame.setVisible(true);
-            }
+            JTextArea manual1 = new JTextArea("Hallo! Herzlich willkommen zu 'MAXX'!"
+                    + "\nIn unserem Spiel können vier Spieler abwechselnd miteinander spielen. Diese Spieler sind als 'black', 'white', 'red' und 'yellow' konfiguriert."
+                    + "\nAlle Spieler haben einen Spielstein, welchen sie auf dem Spielfeld hin und her bewegen können."
+                    + "\n\nAber Achtung!!"
+                    + "\nDer Spielstein kann nur in 4 bestimmte Richtungen bewegt werden. Nämlich nach: 'NW' 'NO' 'SO' und 'SW'."
+                    + "\nZudem hat jeder Spieler noch einen special move, welcher dem Schwarzen Spieler erlaubt seinen Spielstein nach rechts zu bewegen, "
+                    + "\ndem weißen Spieler ist es erlaubt, seinen nach links zu bewegen, der rote Spieler kann seinen nach oben und der gelbe Spieler seinen nach unten bewegen."
+                    + "\nDieser move wird unter 'SPECIAL' abgerufen!"
+                    + "\nGewinner des Spiels ist derjenige, der mehr oder gleich 42 Punkte hat!\n");
+            panel.add(manual1);
+            myJFrame.setSize(900, 300);
+            myJFrame.add(panel);
+            myJFrame.setVisible(true);
         });
 
         //this ActionListener close the whole program
@@ -94,32 +97,28 @@ public class MainMenu extends JFrame implements Runnable {
             }
         });
 
-        play.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (playerSelection.getSelectedIndex() == 0) {
-                    GameBoard myBoard0 = new GameBoard(2,player);
-                } else if (playerSelection.getSelectedIndex() == 1) {
-                    GameBoard myBoard1 = new GameBoard(3,player);
-                } else if (playerSelection.getSelectedIndex() == 2) {
-                    GameBoard myBoard2 = new GameBoard(4,player);
-                }
+        play.addActionListener(e -> {
+            if (playerSelection.getSelectedIndex() == 0) {
+                GameBoard myBoard0 = new GameBoard(2,players);
+            } else if (playerSelection.getSelectedIndex() == 1) {
+                GameBoard myBoard1 = new GameBoard(3,players);
+            } else if (playerSelection.getSelectedIndex() == 2) {
+                GameBoard myBoard2 = new GameBoard(4,players);
             }
         });
-        Thread thread = new Thread(() -> run());
+        Thread thread = new Thread(this);
         thread.start();
     }
 
     @Override
     public void run() {
-        play.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (playerSelection.getSelectedIndex() == 0) {
-                    //GameBoard myBoard0 = new GameBoard(2);
-                } else if (playerSelection.getSelectedIndex() == 1) {
-                    //  GameBoard myBoard1 = new GameBoard(3);
-                } else if (playerSelection.getSelectedIndex() == 2) {
-                    // GameBoard myBoard2 = new GameBoard(4);
-                }
+        play.addActionListener(e -> {
+            if (playerSelection.getSelectedIndex() == 0) {
+                //GameBoard myBoard0 = new GameBoard(2, players);
+            } else if (playerSelection.getSelectedIndex() == 1) {
+                //GameBoard myBoard1 = new GameBoard(3, players);
+            } else if (playerSelection.getSelectedIndex() == 2) {
+                //GameBoard myBoard2 = new GameBoard(4, players);
             }
         });
     }
