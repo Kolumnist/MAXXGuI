5948df63933d8a8e8ac20ed084d8c69de78e7eed
Kolumnist <96417832+Kolumnist@users.noreply.github.com>
2022-04-21 17:10:06 +0200

    omg look at this gorgeous method

--- src/MAXX.java
+++ src/MAXX.java
@@ -11,127 +11,99 @@ public class MAXX implements MouseListener {
 
     public static Scanner sc = new Scanner(System.in); // create a scanner, so people can enter a text by the keyboard
     public static GameBoard gb = new GameBoard(8, 8); // gameboard object
-    public static int playerNumber = 2;  // create a player number -> at the beginn no players are selected so the number is 0
+
+    public static int selected = 0; //0-3 First to Last Player
+    public static int playerNumber = 2;  // create a player number -> at the beginning there are 2 players selected
+    public static Player white;
+    public static Player black;
+    public static Player red;
+    public static Player yellow;
+    public static Player[] players = new Player[playerNumber];
+
     public static boolean repeat = true; // create a boolean variable
     public static String s;  // create a String named s
 
 
-    public void mouseClicked(MouseEvent e)
-    {
-        if(player.getX_pos() == field.getX()+1 && player.getY_pos() == field.getY()+1)
+    public void mouseClicked(MouseEvent e) {
+        if (players[selected].getX_pos() == ((Field) e.getComponent()).getX() + 1 && players[selected].getY_pos() == ((Field) e.getComponent()).getY() + 1)
         {
-            player.northWest();
-            player.player_value.add(field.fieldValue);
+            players[selected].northWest();
+            players[selected].player_value.add(((Field) e.getComponent()).fieldValue);
         }
-        else if(player.getX_pos() == field.getX()-1 && player.getY_pos() == field.getY()+1)
+        else if (players[selected].getX_pos() == ((Field) e.getComponent()).getX() - 1 && players[selected].getY_pos() == ((Field) e.getComponent()).getY() + 1)
         {
-            player.southWest();
-            player.player_value.add(field.fieldValue);
+            players[selected].southWest();
+            players[selected].player_value.add(((Field) e.getComponent()).fieldValue);
         }
-        else if(player.getX_pos() == field.getX()-1 && player.getY_pos() == field.getY()-1)
+        else if (players[selected].getX_pos() == ((Field) e.getComponent()).getX() - 1 && players[selected].getY_pos() == ((Field) e.getComponent()).getY() - 1)
         {
-            player.southEast();
-            player.player_value.add(field.fieldValue);
+            players[selected].southEast();
+            players[selected].player_value.add(((Field) e.getComponent()).fieldValue);
         }
-        else if(player.getX_pos() == field.getX()+1 && player.getY_pos() == field.getY()-1)
+        else if (players[selected].getX_pos() == ((Field) e.getComponent()).getX() + 1 && players[selected].getY_pos() == ((Field) e.getComponent()).getY() - 1)
         {
-            player.northEast();
-            player.player_value.add(field.fieldValue);
+            players[selected].northEast();
+            players[selected].player_value.add(((Field) e.getComponent()).fieldValue);
         }
-        else if()
+        else if ()
         {
-            player.special();
-            player.player_value.add(field.fieldValue);
+            players[selected].special();
+            players[selected].player_value.add(((Field) e.getComponent()).fieldValue);
         }
         else
             System.out.println("\n  Das darf deine Figur nicht!");
         //When the player gives something that he cant do
 
-        onPlayerMoves((Field)e.getComponent(), (Field)e.getComponent());
+        onPlayerMoves((Field) e.getComponent(), (Field) e.getComponent());
     }
 
     // This method shows the instruction and create a gameboard with 2/ 3 or 4 Players
     public static void main(String[] args) throws Exception {
-        System.out.println("Möchten Sie die Anleitung sehen?");
-        System.out.println("Bitte geben Sie n/j für nein/ja ein!");
-        s = sc.nextLine();
-        System.out.println("\n");
-        if (!((s.equalsIgnoreCase("n")) || (s.equalsIgnoreCase("j")))) {
-            System.out.println("Sie dürfen nur 'n' oder 'j' eingeben! Bitte geben Sie Ihre Anweisung erneut ein: ");
-            System.out.println("Möchten Sie die Anleitung sehen?");
-            System.out.println("Bitte geben Sie n/j für nein/ja ein!");
-            s = sc.nextLine();
-            System.out.println("\n");
+
+        switch (playerNumber) {
+            case 2:
+                white = new Player(2, 2, 'W');
+                black = new Player(5, 5, 'B');
+                players[0] = white;
+                players[1] = black;
+                break;
+            case 3:
+                white = new Player(2, 2, 'W');
+                black = new Player(5, 5, 'B');
+                red = new Player(5, 2, 'R');
+                players[0] = white;
+                players[1] = black;
+                players[2] = red;
+                break;
+            case 4:
+                white = new Player(2, 2, 'W');
+                black = new Player(5, 5, 'B');
+                red = new Player(5, 2, 'R');
+                yellow = new Player(2, 5, 'Y');
+                players[0] = white;
+                players[1] = black;
+                players[2] = red;
+                players[3] = yellow;
+                break;
         }
-        switch (s.toLowerCase()) {
-            case "j":
-                System.out.println(hello());
-                System.out.println("Bitte drücken Sie enter um das Spiel zu starten! \n");
-                sc.nextLine();
-            case "n":
 
-                try{
-                    System.out.print("Wie viele Spieler?(2-4): ");
-                    setPlayerNumber(sc.nextInt());
-                    while (playerNumber > 4 || playerNumber < 2) {
-                        System.out.print("Es kann nur 2-4 Spieler geben!");
-                        setPlayerNumber(sc.nextInt());
-                    }
-                    System.out.println("\n");
-                }catch(Exception e){
-                    System.out.println("Es wurde keine Zahl eingegeben. Das Spiel startet jetzt automatisch mit 2 Spielern");
-                }
+        sc.nextLine();
+        gb.createBoard(players);
+        gb.drawBoard();
 
 
-                Player white;
-                Player black;
-                Player red;
-                Player yellow;
-                Player[] playerArr = new Player[playerNumber];
+        while (repeat) {
+            for (int i = 0; i < playerNumber && repeat; i++) {
 
-                switch (playerNumber) {
-                    case 2:
-                        white = new Player(2, 2, 'W');
-                        black = new Player(5, 5, 'B');
-                        playerArr[0] = white;
-                        playerArr[1] = black;
-                        break;
-                    case 3:
-                        white = new Player(2, 2, 'W');
-                        black = new Player(5, 5, 'B');
-                        red = new Player(5, 2, 'R');
-                        playerArr[0] = white;
-                        playerArr[1] = black;
-                        playerArr[2] = red;
-                        break;
-                    case 4:
-                        white = new Player(2, 2, 'W');
-                        black = new Player(5, 5, 'B');
-                        red = new Player(5, 2, 'R');
-                        yellow = new Player(2, 5, 'Y');
-                        playerArr[0] = white;
-                        playerArr[1] = black;
-                        playerArr[2] = red;
-                        playerArr[3] = yellow;
-                        break;
-                }
-
-                sc.nextLine();
-                gb.createBoard(playerArr);
-                gb.drawBoard();
-
-
-                while (repeat) {
-                    for (int i = 0; i < playerNumber && repeat; i++) {
-
-                        phase(playerArr[i]);
-                    }
-                    for (Player value : playerArr) {
-                        System.out.println("Spieler: " + value.toString() + " hat " + value.player_value + " Punkte.");
-                    }
-                }
+                phase(players[i]);
+            }
+            for (Player value : players) {
+                System.out.println("Spieler: " + value.toString() + " hat " + value.player_value + " Punkte.");
+            }
         }
     }
+
+}
 
     // this method organizate the whole game history: With this method a player can walk/ he can stop the game or the method prints the victory of an player
     public static void phase(Player p) throws InterruptedException {
@@ -157,12 +129,6 @@ public class MAXX implements MouseListener {
         }
     }
 
-    // this method reads a String and shows it on the keyboard
-    public static String readString(String s) {
-        System.out.print(s);
-        return sc.nextLine();
-    }
-
     // this method stops the programm
     public static void stop() throws InterruptedException {
         repeat = false;
@@ -172,20 +138,6 @@ public class MAXX implements MouseListener {
             System.out.print(".");
         }
         System.out.println("\n");
-    }
-
-    // this method shows the instruction
-    public static String hello() {
-        System.out.println("" + "Hallo! Herzlich willkommen zu 'MAXX'!"
-                + "\nIn unserem Spiel können vier Spieler abwechselnd miteinander spielen. Diese Spieler sind als 'black', 'white', 'red' und 'yellow' konfiguriert."
-                + "\nAlle Spieler haben einen Spielstein, welchen sie auf dem Spielfeld hin und her bewegen können."
-                + "\nAber Achtung!!"
-                + "\nDer Spielstein kann nur in 4 bestimmte Richtungen bewegt werden. Nämlich nach: 'NW' 'NO' 'SO' und 'SW'."
-                + "\nZudem hat jeder Spieler noch einen special move, welcher dem Schwarzen Spieler erlaubt seinen Spielstein nach rechts zu bewegen, "
-                + "dem weißen Spieler ist es erlaubt, seinen nach links zu bewegen, der rote Spieler kann seinen nach oben und der gelbe Spieler seinen nach unten bewegen."
-                + "\nDieser move wird unter 'SPECIAL' abgerufen!"
-                + "\nGewinner des Spiels ist derjenige, der mehr oder gleich 42 Punkte hat!\n");
-        return "";
     }
 
     // this method set an player number
